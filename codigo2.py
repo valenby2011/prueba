85e1a2a1313668c05fe7a44e6e8b9d99373b0bdf
Valen <gatunogaton2517@gmail.com>
2024-11-14 14:18:48 -0500

    arreglado ahora cierra tu rama

--- codigo2.py
+++ codigo2.py
@@ -1,6 +1,4 @@
 nombre = "NO HOLA"
 print("hola " + nombre)
 num = int(input("q quieres q sume con 5? si lo se es raro pero debo alargar este codigo pq mi jefe me odia xdxdxdxdxddxd"))
-print(f"aqui esta tu numero mas 5")
-print("asi y esto es pa alargar xd")
-print(f"OH verdad tu numero aqui esta {num}")
+print(f"aqui esta tu numero mas 5 asi y esto es pa alargar xd OH verdad tu numero aqui esta {num}")
