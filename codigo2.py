79ec3a14926b5a6b773c40042e83437c560879de
Valen <gatunogaton2517@gmail.com>
2024-11-14 14:11:49 -0500

    alargando el maldito codigo

--- codigo2.py
+++ codigo2.py
@@ -1,2 +1,6 @@
 nombre = "NO HOLA"
 print("hola " + nombre)
+num = int(input("q quieres q sume con 5? si lo se es raro pero debo alargar este codigo pq mi jefe me odia xdxdxdxdxddxd"))
+print(f"aqui esta tu numero mas 5")
+print("asi y esto es pa alargar xd")
+print(f"OH verdad tu numero aqui esta {num}")
